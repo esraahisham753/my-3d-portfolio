668985671db135c37ab6dd4167d5ea0dd36afd76
Esraa Abduallah <esraahisham753@gmail.com>
2024-09-14 15:31:45 +0000

    add basic geometry, orbit control, animation and resize handler

--- vite/main.js
+++ vite/main.js
@@ -1,24 +1,55 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('canvas');
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshStandardMaterial({color: '#B4B4B3', emissive: '#B4B4B3'});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+const light = new THREE.SpotLight('#F5F5F5', 100);
+light.position.set(1, 1, 1);
+
+scene.add(light);
+
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.pixelRatio = window.devicePixelRatio;
+renderer.render(scene, camera);
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping(true);
+orbitControls.dampingFactor = 0.05;
+orbitControls.enablePan(true);
+orbitControls.enableZoom(true);
+orbitControls.update();
+
+function animate() {
+  requestAnimationFrame(() => {
+    orbitControls.update();
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+    box.rotateY+= 0.1;
+  });
+}
+
+animate();
+
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  
+})
