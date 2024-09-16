2762c022c72fb303ba300e9bffb19c76e1e3745f
Esraa Abduallah <esraahisham753@gmail.com>
2024-09-16 12:52:31 +0000

    ceate basic cylider with sparkles using React three fiber and React three drei and create vite react portfolio

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,10 +1,40 @@
-import {Canvas, useFrame} from '@react-three/fiber';
-import {} from '@react-three/drei';
+import { OrbitControls, Sparkles } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if(meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  })
+
+  return <mesh ref={meshRef}>
+    <cylinderGeometry args={[1, 1, 1]}/>
+    <meshLambertMaterial color={'#468585'} emissive={'#468585'}/>
+    <Sparkles count={100} scale={1} size={6} color={'orange'} speed={0.002}/> 
+  </mesh>;
+};
 
 const App = () => {
   return (
-    <div>
-      Hello, three.js
-    </div>
-  )
-}
+    <Canvas
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+        <OrbitControls enableZoom enablePan enableRotate />
+        <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6}/>
+        <color attach='background' args={['#F0F0F0']}/>
+        <RotatingCube />
+      </Canvas>
+  );
+};
+
+export default App;
